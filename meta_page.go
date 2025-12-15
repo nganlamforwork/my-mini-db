ad8fa77ec3ce6a000e2d2b7f75eb9fd12b1d6b8f
Le Vu  Ngan Lam <nganlamforwork@gmail.com>
2025-12-15 12:23:53 +0700

    feat: intit meta page

---
+++ meta_page.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+
+)
+
+type MetaPage struct {
+	Header PageHeader
+
+	// B+Tree metadata
+	RootPage  uint64 // page id of root
+	PageSize  uint32 // e.g. 4096
+	Order     uint16 // B+Tree order
+	Version   uint16 // format version
+}
+
+func (m *MetaPage) WriteToBuffer(buf *bytes.Buffer) error {
+	// write header first
+	if err := m.Header.WriteToBuffer(buf); err != nil {
+		return err
+	}
+
+	// write meta payload
+	if err := binary.Write(buf, binary.BigEndian, m.RootPage); err != nil {
+		return err
+	}
+	if err := binary.Write(buf, binary.BigEndian, m.PageSize); err != nil {
+		return err
+	}
+	if err := binary.Write(buf, binary.BigEndian, m.Order); err != nil {
+		return err
+	}
+	if err := binary.Write(buf, binary.BigEndian, m.Version); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *MetaPage) ReadFromBuffer(buf *bytes.Reader) error {
+	// read header first
+	if err := m.Header.ReadFromBuffer(buf); err != nil {
+		return err
+	}
+
+	// read meta payload
+	if err := binary.Read(buf, binary.BigEndian, &m.RootPage); err != nil {
+		return err
+	}
+	if err := binary.Read(buf, binary.BigEndian, &m.PageSize); err != nil {
+		return err
+	}
+	if err := binary.Read(buf, binary.BigEndian, &m.Order); err != nil {
+		return err
+	}
+	if err := binary.Read(buf, binary.BigEndian, &m.Version); err != nil {
+		return err
+	}
+
+	return nil
+}
